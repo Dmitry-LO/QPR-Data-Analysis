15dea82249357001d366fa14d37346be79af054c
Dmitry-LO <dmitry.tikhonov.postHZB@gmail.com>
2024-12-05 13:07:24 +0100

    home update

--- funclib/importf.py
+++ funclib/importf.py
@@ -3,6 +3,17 @@ import pandas as pd
 import numpy as np
 import matplotlib.pyplot as plt
 
+class color:
+   PURPLE = '\033[95m'
+   CYAN = '\033[96m'
+   DARKCYAN = '\033[36m'
+   BLUE = '\033[94m'
+   GREEN = '\033[92m'
+   YELLOW = '\033[93m'
+   RED = '\033[91m'
+   BOLD = '\033[1m'
+   UNDERLINE = '\033[4m'
+   END = '\033[0m'
 
 class HandleTest:
     def __init__(self,Path):
@@ -20,12 +31,22 @@ class HandleTest:
                 header=0,
                 na_values="NaN",
             )
-            nfilelist.append(procfile)
-        nfile=pd.concat(nfilelist, ignore_index=True)
-        nfile['Date'] = pd.to_datetime(nfile['Date'] + ' ' + nfile['Time'], format='%Y/%m/%d %H:%M:%S')
-        nfile.drop(columns=["Time"], inplace=True)
-        nfile.rename(columns={"Date":"Date_Time"}, inplace=True)
-        return nfile
-    
+            
+            if procfile.isna().all().all():
+                print(f"File {file_path} contains an all-NaN DataFrame and will be skipped.")
+            else:
+                nfilelist.append(procfile)
+
+
+        try:
+            nfile=pd.concat(nfilelist, ignore_index=True)
+            nfile['Date'] = pd.to_datetime(nfile['Date'] + ' ' + nfile['Time'], format='%Y/%m/%d %H:%M:%S')
+            nfile.drop(columns=["Time"], inplace=True)
+            nfile.rename(columns={"Date":"Date_Time"}, inplace=True)
+            return nfile
+        except Exception as e:
+            print(color.BOLD + color.RED + "Exeption raised: " + color.END + color.END + str(e))
+            nfile=pd.DataFrame()
+            return nfile
     def LoadRecalc(self):
         pass
